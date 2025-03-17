e9fcc41708de964fe7e5046c3c75ffc58717357d
Simon339 <tshepohotmail00@gmail.com>
2025-03-17 18:32:56 +0200

    migrate reset

--- migrations/0001_kind_stingray.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "tokens" DROP CONSTRAINT "tokens_token_unique";--> statement-breakpoint
-ALTER TABLE "tokens" DROP CONSTRAINT "tokens_email_token_pk";--> statement-breakpoint
-CREATE UNIQUE INDEX "tokens_unique_email_token" ON "tokens" USING btree ("email","token");
--- migrations/0002_watery_bloodstorm.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "project_techstacks" ALTER COLUMN "project_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "project_techstacks" ALTER COLUMN "techstack_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "role" SET DATA TYPE role;
