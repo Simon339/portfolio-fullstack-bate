8846e7b7eca3708ad51621bb8b784ca30fce9836
Malesela Simon Malapane <65837866+Simon339@users.noreply.github.com>
2026-05-10 23:40:32 +0200

    Update getcurrectuser.ts

--- hooks/getcurrectuser.ts
+++ hooks/getcurrectuser.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { lastLoginMethodClient, twoFactorClient,  adminClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000" || "http://192.168.1.7:3000",
+    baseURL: process.env.BETTER_AUTH_URL || "https://m-s-portfolio.vercel.app",
     plugins: [ lastLoginMethodClient(), twoFactorClient({ onTwoFactorRedirect(){ window.location.href ="/two-factor" } }), adminClient()]
 })
 
