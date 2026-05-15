7f1583e48e1be03682cd9a39de96fdaac59010a1
Malesela Simon Malapane <65837866+Simon339@users.noreply.github.com>
2026-05-15 06:12:27 +0200

    Update baseURL configuration for authClient

--- hooks/getcurrectuser.ts
+++ hooks/getcurrectuser.ts
@@ -2,11 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { lastLoginMethodClient, twoFactorClient,  adminClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-<<<<<<< HEAD
-    baseURL: process.env.BETTER_AUTH_URL || "https://m-s-portfolio.vercel.app",
-=======
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000" || "https://m-s-portfolio.vercel.app", 
->>>>>>> 1103396 (Describe your changes)
+    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000" || "https://m-s-portfolio.vercel.app",
     plugins: [ lastLoginMethodClient(), twoFactorClient({ onTwoFactorRedirect(){ window.location.href ="/two-factor" } }), adminClient()]
 })
 
